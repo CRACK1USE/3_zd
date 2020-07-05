441ec773cbd43e0fa8d94b74b73eee3094dc9ff8
Антон Cурков <toni.surkov.17@mail.ru>
2020-07-05 14:56:12 +0400

    Добавьте файлы проекта.

---
+++ 3 zadanie/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Console.WriteLine("Введите текст:");
+            text = Convert.ToString(Console.ReadLine());
+
+            while (text.Contains("  "))
+                text = text.Replace("  ", " ");
+
+            Console.WriteLine("Отредактированный текст: " + text);
+            Console.ReadKey();
+        }
+    }
+}
